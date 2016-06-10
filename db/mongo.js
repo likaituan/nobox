716bf34375d10d44218f6c36a15dcea89e48183e
likaituan <61114579@qq.com>
2016-06-11 02:15:15 +0800

    0.3.8

--- db/mongo.js
+++ db/mongo.js
@@ -7,7 +7,6 @@
     var mongodb = {};
     try{
         mongodb = req("mongodb").MongoClient;
-        console.log(mongodb);
         console.log("you are already install mongodb");
     }catch(e){
         console.log("you don't install mongodb");
